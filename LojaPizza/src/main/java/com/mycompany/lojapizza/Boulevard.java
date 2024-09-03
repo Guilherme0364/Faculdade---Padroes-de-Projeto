6a54419f86d89b3329121cfa5b52c40f0394ae80
Guilherme Dias Gregorio <136377466+Guilherme0364@users.noreply.github.com>
2024-09-02 22:29:29 -0300

    Add files via upload

---
+++ LojaPizza/src/main/java/com/mycompany/lojapizza/Boulevard.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lojapizza;
+
+/**
+ *
+ * @author android
+ */
+public class Boulevard{
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
